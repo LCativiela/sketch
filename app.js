08e774bc8d527933d43bd9e1b62eba2def65869a
LCativiela <lorenzocativiela@gmail.com>
2022-04-25 18:26:23 +0200

    Base structure

---
+++ app.js
@@ -0,0 +1,13 @@
+const grid = document.querySelector('.grid');
+let squareSize = 8;
+
+
+createGrid(squareSize);
+
+function createGrid(squareSize){
+    for(let i=0;i<squareSize;i++){
+        for(let j=0;j<squareSize;j++){
+            
+        }
+    }
+}
