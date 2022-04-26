6d5fa4dda2c31fc476edc6b10f9bd5bf5f35b6ca
LCativiela <lorenzocativiela@gmail.com>
2022-04-26 20:24:21 +0200

    Added basic paint functionality

--- app.js
+++ app.js
@@ -1,13 +1,38 @@
 const grid = document.querySelector('.grid');
-let squareSize = 8;
+let gridSize = 18;
 
+createGrid(gridSize);
 
-createGrid(squareSize);
+function createDiv(size){
+    const div = document.createElement('div');
+    div.classList.add('box');
+    div.style.width = `${size}px`;
+    div.style.height = `${size}px`;
 
-function createGrid(squareSize){
-    for(let i=0;i<squareSize;i++){
-        for(let j=0;j<squareSize;j++){
-            
-        }
+    return div;
+}
+
+function createGridColumn(gridSize){
+    const div = document.createElement('div');
+    div.classList.add('gridColumn');
+    
+    
+    for (let i = 0; i < gridSize; i++){
+        div.appendChild (createDiv(grid.clientHeight / gridSize));
+    }
+
+    return div;
+}
+
+function createGrid(gridSize){
+    for ( let i = 0; i < gridSize; i++){
+        grid.appendChild (createGridColumn(gridSize));
     }
 }
+
+grid.addEventListener('mouseover', function (e) {
+    // Add the "active" class to only divs with a "box" class
+    if (e.target.matches('.box')) {
+      e.target.classList.add('active');
+    }
+  });
